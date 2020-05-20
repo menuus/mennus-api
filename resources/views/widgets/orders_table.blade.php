495dc7b42ac23468ac00d0a0f35584b2d1980322
Rodrigo Félix <rodrigopfelix@hotmail.com>
2020-05-20 01:07:01 -0300

    Orders table improvements and bugfix

--- resources/views/widgets/orders_table.blade.php
+++ resources/views/widgets/orders_table.blade.php
@@ -17,8 +17,8 @@
         <tr>
             <td class="text-center">
                 <div class="c-avatar">
-                    @if($order->user->profile->image) //TODO: não está carregando a imagem
-                    <img class="c-avatar-img" src="{{ $order->user->profile->image }}" alt={{ $order->user->email }}>
+                    @if($order->user->profile && $order->user->profile->image)
+                    <img class="c-avatar-img" src="{{ $order->user->profile->image->path }}" alt={{ $order->user->email }}>
                     @else
                     <svg class="c-avatar-img" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                         <path d="M13.468 12.37C12.758 11.226 11.195 10 8 10s-4.757 1.225-5.468 2.37A6.987 6.987 0 008 15a6.987 6.987 0 005.468-2.63z" />
@@ -63,7 +63,9 @@
             </td>
         </tr>
         @empty
-        <tr>Sem ordens</tr>
+        <tr>
+            <td class="text-center" colspan="4">Sem pedidos</td>
+        </tr>
         @endforelse
 
         <!-- <tr>
